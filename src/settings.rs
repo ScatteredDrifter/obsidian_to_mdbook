b7c5bafbad8ff7c5adcacf200d8013fb57992341
ScatteredDrifter <evelyn_stng@protonmail.com>
2024-10-09 17:30:21 +0200

    adding settings for easier configuration

---
+++ src/settings.rs
@@ -0,0 +1,11 @@
+// File contains settings for modifying behavior of this "parser"
+
+// denotes whether to ask for Paths or take the ones given in settings!
+pub const REQUEST_PATHS: bool = false;
+// denotes root path for obsidian vault
+pub const PATH_SOURCE: &str = "Path/To/Obsidian/Structure";
+// denotes root path of destination
+pub const PATH_DEST: &str = "Path/To/Save/To";
+pub const PATH_SUMMARY: &str = "Path/To/Save/Summary.md";
+
+pub const PRINT_DEBUG:bool = false;
